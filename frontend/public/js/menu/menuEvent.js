9d8f4f5106769f2cd34abead5d6f8d698a5c2d1e
secretjuju <dev.tmdqh@gmail.com>
2021-07-14 19:03:53 +0900

    add: 메뉴의 탭을 누르면 위치가 바뀜

--- frontend/public/js/menu/menuEvent.js
+++ frontend/public/js/menu/menuEvent.js
@@ -3,33 +3,49 @@ const elements = {};
 const tabList = {
   productList: {
     number: 1,
+    sectionId: 'product-list-window',
   },
   chatting: {
     number: 2,
+    sectionId: 'chat-list-window',
   },
   interestList: {
     number: 3,
+    sectionId: 'interest-list-window',
   },
 };
+
+const showTab = () => {};
 
 const menuSectionClickHandler = (event) => {
   // TODO change parameter without redirect
   // 리다이렉트가 없어진다면 서로 슬라이드 해서 넘길 수 있음
-  // event.preventDefault();
+  const id = event.currentTarget.id;
+  const tabNames = Object.keys(tabList);
+  if (!tabNames.includes(id)) {
+    event.preventDefault();
+    return null;
+  }
+
+  let left = tabList[id]?.number;
+  left = -1 * ((left - 1) * 100); // left 퍼센트 정하기
+  const $sectionWrapper = document.querySelector(`.section-wrapper`);
+  $sectionWrapper.style.left = `${left}%`;
+  event.preventDefault();
 };
 
-const getTabNumber = (tab) => {
+const getTabInfo = (tab) => {
   const tabNames = Object.keys(tabList);
   if (!tabNames.includes(tab)) {
     return null;
   }
 
-  return tabList[tab]?.number;
+  return tabList[tab];
 };
 
-const setBottomBorder = (tabNumber) => {
+const setBottomBorder = (tabInfo) => {
   const $targetElement = document.querySelector(
-    `.tab-bar > :nth-child(${tabNumber})`,
+    `.tab-bar > :nth-child(${tabInfo.number})`,
   );
   $targetElement.style.borderBottom = '2px solid #2ac1bc';
 };
@@ -37,13 +53,13 @@ const setBottomBorder = (tabNumber) => {
 window.onload = () => {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const { tab } = Object.fromEntries(urlSearchParams.entries());
-  const tabNumber = getTabNumber(tab);
+  const tabInfo = getTabInfo(tab);
 
-  if (tabNumber === null) {
+  if (tabInfo === null) {
     return (location.href = './menu?tab=productList');
   }
 
-  setBottomBorder(tabNumber);
+  setBottomBorder(tabInfo);
 
   const menuSections = document.querySelectorAll('.tab-bar > section');
 
