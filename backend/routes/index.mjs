728f66fdc39b36943fa160eb7edb88aee95c707d
peanut-lover <jihoson94@gmail.com>
2021-07-16 22:45:19 +0900

    fix: small fix

--- backend/routes/index.mjs
+++ backend/routes/index.mjs
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.redirect("/index");
+  res.render("index");
 });
 
 export default router;
