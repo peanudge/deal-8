e7da62fdbde9ad149c70f72c0e219db58e4247a2
peanut-lover <jihoson94@gmail.com>
2021-07-18 11:17:44 +0900

    add: 내동네 설정 화면 기능 구현

--- frontend/src/page/LocationPage/Controller.js
+++ frontend/src/page/LocationPage/Controller.js
@@ -1,7 +1,95 @@
 export default class Controller {
-  constructor() {
+  constructor(
+    store,
+    { locationListView, deleteLocationModalView, addLocationModalView }
+  ) {
+    this.store = store;
+
+    this.deleteLocationModalView = deleteLocationModalView;
+    this.addLocationModalView = addLocationModalView;
+    this.locationListView = locationListView;
+
+    this.isShowDeleteLocationModal = false;
+    this.isShowAddLocationModal = false;
+
+    this.subscribeViewEvents();
+    this.render();
+  }
+  subscribeViewEvents() {
+    this.locationListView.on("@click-delete-location", (e) => {
+      this.showDeleteLocationModal(e.detail.value);
+    });
+
+    this.locationListView.on("@click-add-location", (e) =>
+      this.showAddLocationModal()
+    );
+
+    this.deleteLocationModalView.on("@close-delete-modal", (e) => {
+      this.hideDeleteLocationModal();
+    });
+
+    this.deleteLocationModalView.on("@delete-location", (e) => {
+      this.deleteLocation();
+      this.hideDeleteLocationModal();
+    });
+
+    this.addLocationModalView.on("@close-add-modal", (e) => {
+      this.hideAddLocationModal();
+    });
+
+    this.addLocationModalView.on("@add-location", (e) => {
+      const location = e.detail.value;
+      this.addLocation(location);
+      this.hideAddLocationModal();
+    });
+  }
+
+  addLocation(location) {
+    // TODO: CAll Location 추가 API
+    console.log(location + " 추가");
+  }
+
+  deleteLocation() {
+    // TODO: CAll Location Delete API
+    console.log(this.store.targetLocation + " 제거");
+  }
+
+  showDeleteLocationModal(location) {
+    this.isShowDeleteLocationModal = true;
+    this.store.targetLocation = location;
     this.render();
   }
 
-  render() {}
+  hideDeleteLocationModal() {
+    this.isShowDeleteLocationModal = false;
+    this.render();
+  }
+
+  showAddLocationModal() {
+    this.isShowAddLocationModal = true;
+    this.render();
+  }
+
+  hideAddLocationModal() {
+    this.isShowAddLocationModal = false;
+    this.render();
+  }
+
+  render() {
+    const { targetLocation } = this.store;
+
+    this.locationListView.show();
+
+    if (this.isShowAddLocationModal) {
+      this.addLocationModalView.show();
+    } else {
+      this.addLocationModalView.hide();
+    }
+
+    if (this.isShowDeleteLocationModal) {
+      this.deleteLocationModalView.show(targetLocation);
+    } else {
+      this.deleteLocationModalView.hide();
+    }
+  }
 }
