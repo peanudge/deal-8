e48f62bee279898d82348c4e3a311890a8e87266
peanut-lover <jihoson94@gmail.com>
2021-07-18 21:16:37 +0900

    add: location API 추가

--- backend/src/model/Account/AbstractAccountStore.js
+++ backend/src/model/Account/AbstractAccountStore.js
@@ -1,4 +1,6 @@
 export default class AbstractProductStore {
   async getAccount(username) {}
   async createAccount({ username, locations }) {}
+  async addLocation(username, location) {}
+  async removeLocation(username, location) {}
 }
