1f9b1e58ae5839b1ed048f5339ea06d23b4ff889
peanut-lover <jihoson94@gmail.com>
2021-07-18 21:16:37 +0900

    add: location API 추가

--- backend/src/routes/api/account/index.js
+++ backend/src/routes/api/account/index.js
@@ -1,5 +1,9 @@
 import express from "express";
-
+import {
+  BAD_REQUEST,
+  SUCCESS_STATUS,
+  UNAUTHORIZED_STATUS,
+} from "../../../util/HttpStatus.js";
 import AccountStore from "../../../model/Account/Store/InMemmoryAccountStore.js";
 const accountStore = new AccountStore();
 
@@ -11,6 +15,49 @@ router.get("/me", async (req, res) => {
     res.json({ isAuth: true, account });
   } else {
     res.json({ isAuth: false });
+  }
+});
+
+router.post("/me/location", async (req, res) => {
+  const { location } = req.body;
+
+  if (req.session["username"]) {
+    const username = req.session["username"];
+    const account = await accountStore.getAccount(username);
+    if (account.locations.length < 2) {
+      const account = await accountStore.addLocation(username, location);
+      res.status(SUCCESS_STATUS).json({ success: true, account });
+    } else {
+      res.status(BAD_REQUEST).json({
+        success: false,
+        error: "지역 정보는 최대 2개까지만 등록할 수 있습니다.",
+      });
+    }
+  } else {
+    res
+      .status(UNAUTHORIZED_STATUS)
+      .json({ success: false, error: "로그인이 필요합니다." });
+  }
+});
+
+router.delete("/me/location", async (req, res) => {
+  const { location } = req.query;
+  if (req.session["username"]) {
+    const username = req.session["username"];
+    const account = await accountStore.getAccount(username);
+    if (account.locations.length >= 1) {
+      const account = await accountStore.removeLocation(username, location);
+      res.status(SUCCESS_STATUS).json({ success: true, account });
+    } else {
+      res.status(BAD_REQUEST).json({
+        success: false,
+        error: "지역 정보는 최소한 하나는 있어야합니다.",
+      });
+    }
+  } else {
+    res
+      .status(UNAUTHORIZED_STATUS)
+      .json({ success: false, error: "로그인이 필요합니다." });
   }
 });
 
