12e2d8f05440f858d5e219f18712cbba70a652a4
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-07-18 11:50:20 +0900

    add: product router 추가

---
+++ backend/src/apis/product.js
@@ -0,0 +1,34 @@
+const getProducts = async (req, res) => {
+  res.send("test");
+};
+
+const getProduct = async (req, res) => {
+  res.send("test");
+};
+
+const getCategories = async (req, res) => {
+  res.send("test");
+};
+
+const uploadFile = async (req, res) => {
+  res.send("test");
+};
+
+const updateProduct = async (req, res) => {
+  res.send("test");
+};
+
+const deleteProduct = async (req, res) => {
+  res.send("test");
+};
+
+const productApi = {
+  getProducts,
+  getProduct,
+  getCategories,
+  uploadFile,
+  updateProduct,
+  deleteProduct,
+};
+
+export default productApi;
--- backend/src/routes/apis/product.js
+++ backend/src/routes/apis/product.js
@@ -1,7 +1,24 @@
 import express from "express";
+import productApi from "../../apis/product.js";
 
 const router = express.Router();
 
+/*
+GET /api/product?location=[주소(동)]&category=[카테고리id]
+GET /api/product/detail?id=[productId]
+GET /api/product/category
+PUT /api/product/media
+PUT /api/product
+POST /api/product/
+DELETE /api/product?id=[상품아이디]
+*/
+
 // routing
 
+router.get("/", productApi.getProducts);
+router.get("/detail", productApi.getProduct);
+router.get("/category", productApi.getCategories);
+router.put("/media", productApi.uploadFile);
+router.post("/", productApi.updateProduct);
+router.delete("/", productApi.deleteProduct);
 export default router;
