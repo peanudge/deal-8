5fa06b5553e74b5162ad7cd9aa330434e702ad1f
peanut-lover <jihoson94@gmail.com>
2021-07-19 20:29:22 +0900

    add: category store and sample data script

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,8 @@ import multer from "multer";
 import mysqlConnection from "./config/mysql.js";
 import indexRouter from "./routes/index.js";
 
+import { createMockData } from "./mock/mockMySQLData.js";
+
 const __dirname = path.resolve();
 
 // Mysql Setting
@@ -16,7 +18,7 @@ const __dirname = path.resolve();
 mysqlConnection.connect(function (err) {
   // TODO : connection 정보를 남겨야 한다.
   if (err) throw err;
-  console.log("database mysqlConnection success");
+  createMockData();
 });
 
 // Multer Setting
